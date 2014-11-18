edcd91b6b8aba94fa64cabe5ba7623d2b4c3f2b8
Liam Sean Brady <liamseanbrady@gmail.com>
2014-11-17 22:53:52 -0500

    Add functionality to add comments to a post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.creator = User.first
+
+    if @comment.save
+      flash[:notice] = 'Your comment was added'
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
