1e290874e41d2a8cf866462af5357cd1ec1d1354
Liam Sean Brady <liamseanbrady@gmail.com>
2014-11-21 21:49:54 -0500

    Add helper methods for routing urls to external websites and displaying a formatted datetime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def fix_url(str)
+  str.starts_with?('http://') ? str : "http://#{str}"
+end
+
+def display_datetime(dt)
+  dt.strftime("%m/%d/%Y %l:%m%P %Z")
+end
+
 end
