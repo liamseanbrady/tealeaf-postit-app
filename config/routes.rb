990853c7c051c8fafd5f60594b2c376310614600
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-02 16:27:44 -0500

    Add vote functionality to posts and route for vote functionality in comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,14 @@ PostitTemplate::Application.routes.draw do
   get '/logout', to: 'sessions#destroy', as: 'logout'
 
   resources :posts, except: [:destroy] do
-    resources :comments, only: [:create]
+    member do
+      post :vote
+    end
+    resources :comments, only: [:create] do
+      member do
+        post :vote
+      end
+    end
   end
   resources :categories, only: [:new, :create, :show]
   resources :users, except: [:index, :new, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201224656) do
+ActiveRecord::Schema.define(version: 20141202175606) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
