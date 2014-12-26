f80d72573e39e97edbeddd46dffb83fcd3ba9493
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-26 10:27:32 +0000

    Add access_denied helper for app-wide general error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :require_user
+  helper_method :current_user, :logged_in?, :require_user, :access_denied
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,9 +14,11 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    if !logged_in?
-      flash[:error] = 'You must be logged in to do that'
-      redirect_to root_path
-    end
+    access_denied unless logged_in?
+  end
+
+  def access_denied
+    flash[:error] = 'You must be logged in to do that'
+    redirect_to root_path
   end
 end
